c8ca037afd701b4db0deca001a85b1cc5b576b5a
fnbca <fidealisn@gmail.com>
2025-03-12 17:21:55 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="Application Fidealis", page_icon="📜")
+
+st.title("🏠 Accueil - Application Fidealis")
+st.sidebar.title("Navigation")
+
+# Affichage d'un message d'accueil
+st.write("Bienvenue dans l'application de gestion de Fidealis.")
+
+st.write("📌 Sélectionnez une page dans le menu de gauche pour commencer.")
